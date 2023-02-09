92328f5436ccbeaa6c90099e329ad6e87ab2d58d
PgmAdmin <prmorais1302@gmail.com>
2023-02-08 22:19:21 -0300

    configuring server and database access

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
+import 'reflect-metadata';
+
+const PORT = process.env.DB_PORT as number | undefined;
+
+export const appDatasource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('TESTE')
+import app from './app';
+import { appDatasource } from './data-source';
+
+const main = async () => {
+  try {
+    await appDatasource.initialize();
+    console.info('Database connected successfully');
+
+    app.listen(process.env.SERVER_PORT, () =>
+      console.log('Server running in the port', process.env.SERVER_PORT),
+    );
+  } catch (err) {
+    console.error('Erro connected database or running server');
+  }
+};
+
+main();
